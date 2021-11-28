b081d645cb0dca9da0ae223fa39db95f74d7005f
Fin Kingma <fin.kingma@praegus.nl>
2021-11-28 14:14:14 +0100

    first commit

---
+++ lib/advent2021/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Advent2021
+  VERSION = "0.1.0"
+end
---
+++ lib/foo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "advent2021/version"
+
+module Advent2021
+  class Error < StandardError; end
+  # Your code goes here...
+end
